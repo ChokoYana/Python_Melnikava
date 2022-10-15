22894b9d66bf32389f888fbfbd6d48c2d81815e1
Yana <yana.melnik2022@mail.ru>
2022-10-15 21:48:13 +0300

    tekst

---
+++ HomeWork02.py
@@ -0,0 +1,41 @@
+# Напишите программу, которая принимает на вход вещественное 
+# число и показывает сумму его цифр.
+
+# a = int(input())
+# sum = 0
+# while a != 0:
+#     sum += a % 10
+#     a //= 10
+# print(sum)
+
+
+# a = input()
+# sum = 0
+# for symbol in a:
+#     if symbol != '.' and symbol != '-':
+#         sum += int(symbol)
+# print(sum)
+
+# Напишите программу, которая принимает на вход число N и 
+# выдает набор произведений чисел от 1 до N.
+
+# n = int(input())
+# array = []
+# f = 1
+# for i in range(1, n +1):
+#     f *= i
+#     array.append(f)
+# print(array)
+
+
+
+
+# Задайте список из n чисел последовательности (n + (1 / n) ** n) 
+# и выведите на экран их сумму.
+
+# n = int(input())
+# ar = []
+# for i in range (1, n+1):
+#     ar.append(round(((1 + 1 / i) ** i),2))
+# print(ar)
+
